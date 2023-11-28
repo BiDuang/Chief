827a09ddffa24b04a42cb98cfcf8d83db4625718
Chien Zhang <zqy0224@live.com>
2023-11-28 22:15:58 +0800

    navigation added

--- src/Chief.App/ViewModelLocator.cs
+++ src/Chief.App/ViewModelLocator.cs
@@ -7,21 +7,24 @@ namespace Chief.App;
 
 public class ViewModelLocator
 {
-    public IServiceProvider Provider { get; init; }
+    public static ViewModelLocator Instance { get; private set; } = null!;
+    public IServiceProvider Provider { get; }
 
     public ViewModelLocator()
     {
         Provider = ConfigureServices();
+
+        Instance = this;
     }
 
     private IServiceProvider ConfigureServices()
     {
         var container = new ServiceCollection();
-        
+
         container.AddSingleton<NavigationService>();
 
         container.AddScoped<HomeViewModel>();
-        
+
         return container.BuildServiceProvider();
     }
 
---
+++ src/Chief.App/Views/ToolchainView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Chief.App.Views;
+
+public partial class ToolchainView : UserControl
+{
+    public ToolchainView()
+    {
+        InitializeComponent();
+    }
+}
